6542794a8c69c97bf6fcf80fd5d0b565e1e393c5
tdayris <thibault.dayris@gustaveroussy.fr>
2023-05-31 18:32:40 +0200

    chipseeker annotate peak

--- workflow/rules/annotation/chipseeker_peak_annotate.smk
+++ workflow/rules/annotation/chipseeker_peak_annotate.smk
@@ -1,4 +1,4 @@
-rule chipseeker_annotate_peak:
+rule chipseeker_annotate_peak_from_ranges:
     input:
         ranges="csaw/results/{comparison_name}.RDS"
     output:
@@ -12,4 +12,29 @@ rule chipseeker_annotate_peak:
     log:
         "logs/chipseeker/annotate/{comparison_name}.log"
     params:
-        organism="hg38" if config[]
+        organism="hg38" if species == "homo_sapiens" else "mm10"
+    conda:
+        "../../envs/chipseeker.yaml"
+    script:
+        "../../scripts/chipseeker/chipseeker_annotate.R"
+
+
+rule chipseeker_annotate_peak_from_bed:
+    input:
+        bed="macs2/callpeak_{peaktype}/{sample}_peaks.{peaktype}Peak.bed"
+    output:
+        rds="chipseeker/annotation/{sample}.{peaktype}.RDS",
+        tsv="data_output/Differential_Binding/{sample}.{peaktype}.tsv",
+    threads: 1
+    resources:
+        mem_mb=lambda wildcards, attempt: attempt * 1024 * 4,
+        runtime=lambda wildcards, attempt: attempt * 20,
+        tmpdir=tmp,
+    log:
+        "logs/chipseeker/annotate/{sample}.{peaktype}.log"
+    params:
+        organism="hg38" if species == "homo_sapiens" else "mm10"
+    conda:
+        "../../envs/chipseeker.yaml"
+    script:
+        "../../scripts/chipseeker/chipseeker_annotate.R"
--- workflow/scripts/chipseeker/chipseeker_annotate.R
+++ workflow/scripts/chipseeker/chipseeker_annotate.R
@@ -14,6 +14,79 @@ sink(log_file, type = "message")
 
 # Load libraries
 base::library(package = "ChIPseeker", character.only = TRUE)
+
 base::library(package = "org.Hs.eg.db", character.only = TRUE)
 base::library(package = "org.Mm.eg.db", character.only = TRUE)
+
+base::library(package = "EnsDb.Hsapiens.v86", character.only = TRUE)
+base::library(package = "EnsDb.Mmusculus.v79", character.only = TRUE)
 base::message("Libraries loaded")
+
+organism <- "hg38"
+if "organism" %in% base::names(x = snakemake@params) {
+    organism <- base::as.character(x = snakemake@params[["organism"]])
+}
+
+organism <- base::tolower(x = organism)
+if (organism == "hg38") {
+    anno_db <- org.Hs.eg.db
+    edb <- EnsDb.Hsapiens.v86
+} else if (organism == "mm10") {
+    anno_db <- org.Mm.eg.db
+    edb <- EnsDb.Mmusculus.v79
+} else {
+    base::stop("Unknown organism annotation")
+}
+seqlevelsStyle(edb) <- "UCSC"
+
+ranges <- NULL
+if ("ranges" %in% base::names(x = snakemake@input)) {
+    ranges <- base::readRDS(file = base::as.character(x = snakemake@input[["ranges"]]))
+} else {
+    ranges <- base::as.character(x = snakemake@input[["bed"]])
+}
+base::message("Parameters and data loaded")
+
+
+# Building command line
+extra <- "peak = ranges, annoDb = anno_db, TxDb = edb"
+if ("extra" %in% base::names(x = snakemake@params)) {
+    extra <- base::paste(
+        extra,
+        base::as.character(x = snakemake@params[["extra"]]),
+        sep = ", "
+    )
+}
+
+command <- base::paste0(
+    "ChIPseeker::annotatePeak(",
+    extra,
+    ")"
+)
+base::message("Command line used:")
+base::message(command)
+
+# Annotating
+annotation <- base::eval(base::parse(text = command))
+
+
+# Saving results
+base::saveRDS(
+    object = annotation,
+    file = base::as.character(x = snakemake@output[["rds"]])
+)
+
+utils::write.table(
+    base::as.data.frame(annotation),
+    sep = "\t",
+    col.names = TRUE,
+    row.names = TRUE,
+    file = base::as.character(x = snakemake@output["tsv"])
+)
+base::message("Provess over")
+
+
+# Proper syntax to close the connection for the log file
+# but could be optional for Snakemake wrapper
+base::sink(type = "message");
+base::sink();
