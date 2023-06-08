16b43ae3def3c174e5e1e9a667cb28206758bf7d
tdayris <thibault.dayris@gustaveroussy.fr>
2023-06-08 15:43:36 +0200

    frip graph initial commit

---
+++ workflow/scripts/multiqc/multiqc_frip.py
@@ -0,0 +1,34 @@
+#! coding: utf-8
+
+import yaml
+
+results = {
+    "custom_data": {
+        "frip_score": {
+            "id": "frip_score",
+            "section_name": "FRiP Score",
+            "description": (
+                "FRiP (Fragment of Read in Peaks) is a common "
+                "quality control performed right after peak-calling. "
+                "The higher the FRiP score is, the more specific "
+                "was the signal over noise."
+            ),
+            "plot_type": "bargraph",
+            "pconfig": {
+                "id": "frip_score_barplot",
+                "title": "Fragment of Read in Peaks",
+                "ylab": "Percent of reads in Peaks",
+            },
+            "data": {}
+        }
+    }
+}
+
+with open(snakemake.input["frip_tsv"], "r") as frip_stream:
+    for idx, line in enumerate(frip_stream):
+        if idx == 0:
+            continue
+        chomp = line[:-1].split("\t")
+        results["custom_data"]["data"][chomp[0]] = chomp[-1]
+
+with open(snakemake.output[""])
