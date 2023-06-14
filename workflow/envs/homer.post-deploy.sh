e99cc415eab98fed64a9e58ce7cd91468d9e5699
tdayris <thibault.dayris@gustaveroussy.fr>
2023-06-14 14:18:38 +0200

    install promoter data as well

--- workflow/envs/homer.post-deploy.sh
+++ workflow/envs/homer.post-deploy.sh
@@ -1,4 +1,9 @@
 set -eo pipefail
 
+# Install genome data
 perl "${CONDA_PREFIX}share/homer/configureHomer.pl" -install hg38 -keepScript
 perl "${CONDA_PREFIX}share/homer/configureHomer.pl" -install mm10 -keepScript
+
+# Install promoter data
+perl "${CONDA_PREFIX}share/homer/configureHomer.pl" -install human-p -keepScript
+perl "${CONDA_PREFIX}share/homer/configureHomer.pl" -install mouse-p -keepScript
