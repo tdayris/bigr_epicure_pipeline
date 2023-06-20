8d38f8c23487ab86c68b27ee8ef277a31bb2e98d
tdayris <thibault.dayris@gustaveroussy.fr>
2023-06-20 10:58:33 +0200

    fix Conda prefix path

--- workflow/envs/homer.post-deploy.sh
+++ workflow/envs/homer.post-deploy.sh
@@ -1,13 +1,13 @@
 set -eo pipefail
 
 # Install organigme data
-perl "${CONDA_PREFIX}share/homer/configureHomer.pl" -install human-o -keepScript
-perl "${CONDA_PREFIX}share/homer/configureHomer.pl" -install mouse-o -keepScript
+perl "${CONDA_PREFIX}/share/homer/configureHomer.pl" -install human-o -keepScript
+perl "${CONDA_PREFIX}/share/homer/configureHomer.pl" -install mouse-o -keepScript
 
 # Install genome data
-perl "${CONDA_PREFIX}share/homer/configureHomer.pl" -install hg38 -keepScript
-perl "${CONDA_PREFIX}share/homer/configureHomer.pl" -install mm10 -keepScript
+perl "${CONDA_PREFIX}/share/homer/configureHomer.pl" -install hg38 -keepScript
+perl "${CONDA_PREFIX}/share/homer/configureHomer.pl" -install mm10 -keepScript
 
 # Install promoter data
-perl "${CONDA_PREFIX}share/homer/configureHomer.pl" -install human-p -keepScript
-perl "${CONDA_PREFIX}share/homer/configureHomer.pl" -install mouse-p -keepScript
+perl "${CONDA_PREFIX}/share/homer/configureHomer.pl" -install human-p -keepScript
+perl "${CONDA_PREFIX}/share/homer/configureHomer.pl" -install mouse-p -keepScript
