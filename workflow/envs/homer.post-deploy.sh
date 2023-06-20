fefc94eb1d8644b0c86d45f99479d1192a557b38
tdayris <thibault.dayris@gustaveroussy.fr>
2023-06-20 09:30:55 +0200

    Add organism general annotation

--- workflow/envs/homer.post-deploy.sh
+++ workflow/envs/homer.post-deploy.sh
@@ -1,5 +1,9 @@
 set -eo pipefail
 
+# Install organigme data
+perl "${CONDA_PREFIX}share/homer/configureHomer.pl" -install human-o -keepScript
+perl "${CONDA_PREFIX}share/homer/configureHomer.pl" -install mouse-o -keepScript
+
 # Install genome data
 perl "${CONDA_PREFIX}share/homer/configureHomer.pl" -install hg38 -keepScript
 perl "${CONDA_PREFIX}share/homer/configureHomer.pl" -install mm10 -keepScript
