7c64d8afe04071c306385e3d45553c32f43b9313
tdayris <thibault.dayris@outlook.fr>
2024-02-22 14:35:30 +0100

    Update bigr_launcher.sh

--- workflow/scripts/misc/bigr_launcher.sh
+++ workflow/scripts/misc/bigr_launcher.sh
@@ -39,54 +39,49 @@ if [ ! -f "workflow/Snakefile" ]; then
         LAUNCHER_PATH=$(readlink -e "$0")
         cp --verbose "${LAUNCHER_PATH}" "workflow/bigr_launcher.sh"
     fi
-else
-    echo "Pipeline already deployed."
-fi
 
-# Build design file if missing
-if [ ! -f "config/design.tsv" ]; then
-    if [ -f "design.tsv" ]; then
-        mv --verbose "design.tsv" "config/design.tsv"
-    elif [ "${LIBRARY}" == "paired" ]; then
-        echo "Looking for pairs of fastq files..."
-        find "data_input/" -type f -name "*q.gz" | \
-            sort | \
-            uniq | \
-            paste - - | \
-            while read R1 R2; do echo -e "$(basename ${R1})\t${R1}\t${R2}"; done | \
-            awk 'BEGIN{FS="\t"; print "Sample_id" FS "Upstream_file" FS "Downstream_file"} {print $0}' \
-        > "config/design.tsv"
-    else
-        echo "Looking for fastq files..."
-        find "data_input/" -type f -name "*q.gz" | \
-            sort | \
-            uniq | \
-            while read R1; do echo -e "$(basename ${R1})\t${R1}"; done | \
-            awk 'BEGIN{FS="\t"; print "Sample_id" FS "Upstream_file"} {print $0}' \
-        > "config/design.tsv"
+    # Build design file if missing
+    if [ ! -f "config/design.tsv" ]; then
+        if [ -f "design.tsv" ]; then
+            mv --verbose "design.tsv" "config/design.tsv"
+        elif [ "${LIBRARY}" == "paired" ]; then
+            echo "Looking for pairs of fastq files..."
+            find "data_input/" -type f -name "*q.gz" | \
+                sort | \
+                uniq | \
+                paste - - | \
+                while read R1 R2; do echo -e "$(basename ${R1})\t${R1}\t${R2}"; done | \
+                awk 'BEGIN{FS="\t"; print "Sample_id" FS "Upstream_file" FS "Downstream_file"} {print $0}' \
+            > "config/design.tsv"
+        else
+            echo "Looking for fastq files..."
+            find "data_input/" -type f -name "*q.gz" | \
+                sort | \
+                uniq | \
+                while read R1; do echo -e "$(basename ${R1})\t${R1}"; done | \
+                awk 'BEGIN{FS="\t"; print "Sample_id" FS "Upstream_file"} {print $0}' \
+            > "config/design.tsv"
+        fi
     fi
 else
-    echo "Design file already available"
+    # Dealing with windows/mac inputs
+    if [ -f "config/design.tsv" ]; then
+        echo "Removing possible Windows complex end of lines..."
+        sed -i 's|\r||g' config/design.tsv
+    fi
+
+    echo "Updating Snakemake-workflow verison if needed..."
+    sed -i "s|tag=\"[^\"]\"|tag=\"${PIPELINE_VERSION}\"|g" "workflow/Snakefile"
+    
+    # Launching pipeline
+    echo "Launching pipeline..."
+    snakemake --snakefile 'workflow/Snakefile' \
+                --profile '/mnt/beegfs/pipelines/unofficial-snakemake-wrappers/profiles/slurm-web/' \
+                --cache \
+                --rerun-incomplete \
+                  --nt "$@"
 fi
 
-# Dealing with windows/mac inputs
-if [ -f "config/design.tsv" ]; then
-    echo "Removing possible Windows complex end of lines..."
-    sed -i 's|\r||g' config/design.tsv
-else
-    echo "Configuration file not available"
-fi
-
-echo "Updating Snakemake-workflow verison if needed..."
-sed -i "s|tag=\"[^\"]\"|tag=\"${PIPELINE_VERSION}\"|g" "workflow/Snakefile"
-
-# Launching pipeline
-echo "Launching pipeline..."
-snakemake --snakefile 'workflow/Snakefile' \
-            --profile '/mnt/beegfs/pipelines/unofficial-snakemake-wrappers/profiles/slurm-web/' \
-            --cache \
-            --rerun-incomplete \
-              --nt "$@"
 
 
 
